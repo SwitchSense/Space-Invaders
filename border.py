a3d27eefd4ef7cdc37cc2b4e6c629754c1c75b62
SwitchSense <exiastrike@hotmail.com>
2022-01-25 14:59:50 +0700

    Initial commit

--- border.py
+++ border.py
@@ -1,41 +1,41 @@
-# import turtle
-#
-#
-# class Border:
-#     def __init__(self):
-#         """Initialization of the Border class with 5 attributes."""
-#         self.color = 'white'
-#         self.ign = turtle.Turtle()
-#         self.score = 0
-#         self.score_pen = turtle.Turtle()
-#         self.border_pen = turtle.Turtle()
-#
-#     def draw(self):
-#         """Draw the white frame called border."""
-#         self.border_pen.penup()
-#         self.border_pen.speed(0)
-#         self.border_pen.color(self.color)
-#         self.border_pen.setposition(-300, -300)
-#         self.border_pen.pendown()
-#         self.border_pen.pensize(3)
-#         for side in range(4):
-#             self.border_pen.fd(600)
-#             self.border_pen.lt(90)
-#         self.border_pen.hideturtle()
-#
-#     def game_score(self, player_name):
-#         """Write initialize game score and player name."""
-#         player_name = f'Player Name: {player_name}'
-#         game_score = 'Score: %s' % self.score
-#         self.ign.penup()
-#         self.score_pen.penup()
-#         self.ign.speed(0)
-#         self.score_pen.speed(0)
-#         self.ign.color(self.color)
-#         self.score_pen.color(self.color)
-#         self.ign.setposition(290, 273)
-#         self.score_pen.setposition(-290, 273)
-#         self.ign.write(player_name, False, align='right', font=('Mono', 14, 'bold'))
-#         self.score_pen.write(game_score, False, align='left', font=('Mono', 14, 'bold'))
-#         self.ign.hideturtle()
-#         self.score_pen.hideturtle()
+import turtle
+
+
+class Border:
+    def __init__(self):
+        """Initialization of the Border class with 5 attributes."""
+        self.color = 'white'
+        self.ign = turtle.Turtle()
+        self.score = 0
+        self.score_pen = turtle.Turtle()
+        self.border_pen = turtle.Turtle()
+
+    def draw(self):
+        """Draw the white frame called border."""
+        self.border_pen.penup()
+        self.border_pen.speed(0)
+        self.border_pen.color(self.color)
+        self.border_pen.setposition(-300, -300)
+        self.border_pen.pendown()
+        self.border_pen.pensize(3)
+        for side in range(4):
+            self.border_pen.fd(600)
+            self.border_pen.lt(90)
+        self.border_pen.hideturtle()
+
+    def game_score(self, player_name):
+        """Write initialize game score and player name."""
+        player_name = f'Player Name: {player_name}'
+        game_score = 'Score: %s' % self.score
+        self.ign.penup()
+        self.score_pen.penup()
+        self.ign.speed(0)
+        self.score_pen.speed(0)
+        self.ign.color(self.color)
+        self.score_pen.color(self.color)
+        self.ign.setposition(290, 273)
+        self.score_pen.setposition(-290, 273)
+        self.ign.write(player_name, False, align='right', font=('Mono', 14, 'bold'))
+        self.score_pen.write(game_score, False, align='left', font=('Mono', 14, 'bold'))
+        self.ign.hideturtle()
+        self.score_pen.hideturtle()
--- screen.py
+++ screen.py
@@ -1,104 +1,104 @@
-# import turtle
-# import random
-# import math
-# import json
-# from border import Border
-# from player import Player
-# from invader import Invader
-#
-#
-# # User input player name and difficulty level.
-# player_name_input = turtle.textinput('Enter player name: ', 'Your name:')
-# while True:
-#     level_input = turtle.textinput('Choose difficulty',  '(Easy, Normal, Hard):')
-#     if level_input.upper() == 'EASY':
-#         mode_input = 10
-#         break
-#     elif level_input.upper() == 'NORMAL':
-#         mode_input = 15
-#         Player().laser_speed = 80
-#         break
-#     elif level_input.upper() == 'HARD':
-#         mode_input = 20
-#         Player().laser_speed = 120
-#         break
-#     else:
-#         print('Please Try again.')
-#
-# border_obj = Border()
-# player_obj = Player()
-# invader_obj = Invader()
-#
-# # Register shapes to turtle.
-# turtle.register_shape("player.gif")
-# turtle.register_shape("invader.gif")
-#
-# # Set up screen.
-# game = turtle.Screen()
-# game.title('Space Invaders')
-# game.bgcolor('black')
-# game.bgpic('background.gif')
-# game.tracer(1)
-#
-# # Set up border, write game score.
-# border_obj.draw()
-# border_obj.game_score(player_name_input)
-#
-# # Set position of player, laser and group of invaders.
-# player_obj.set_icon_position()
-# player_obj.set_laser_position()
-# invader_army = list(invader_obj.set_invaders(mode_input))
-#
-# # Binding key to control player icon and firing option.
-# turtle.listen()
-# turtle.onkey(player_obj.move_left, "Left")
-# turtle.onkey(player_obj.move_right, "Right")
-# turtle.onkey(player_obj.fire, "space")
-#
-#
-# def collision_check(p1, p2):
-#     distance = math.sqrt(math.pow(p1.xcor() - p2.xcor(), 2) + math.pow(p1.ycor() - p2.ycor(), 2))
-#     if distance < 30:
-#         return True
-#     else:
-#         return False
-#
-#
-# # Main game loop
-# while True:
-#     game.update()
-#     for invader in invader_army:
-#         invader_obj.move(invader_army, invader)
-#         # Check if collision between laser and invader.
-#         if collision_check(player_obj.laser, invader):
-#             border_obj.score += 10
-#             game_score = "Score: %s" % border_obj.score
-#             border_obj.score_pen.clear()
-#             border_obj.score_pen.write(game_score, False, align="left", font=("Mono", 14, "bold"))
-#             # Reset laser position and status.
-#             player_obj.laser.hideturtle()
-#             player_obj.laser_status = "ready"
-#             player_obj.laser.setposition(player_obj.icon.xcor(), player_obj.icon.ycor())
-#             # Reset invader position.
-#             x = random.randint(-250, 200)
-#             y = random.randint(100, 250)
-#             invader.setposition(x, y)
-#         # Check if collision between player icon and invader.
-#         if collision_check(player_obj.icon, invader):
-#             player_obj.icon.hideturtle()
-#             invader.hideturtle()
-#             # Write save data file as json file
-#             save_slot = {
-#                 'data_slot': {
-#                     'Player name': player_name_input.upper(),
-#                     'Difficulty': level_input.upper(),
-#                     'Score': border_obj.score
-#                 }
-#             }
-#             with open('leaderboard.json', 'a') as leaderboard_file:
-#                 json.dump(save_slot, leaderboard_file, indent=4)
-#             game_over = turtle.textinput('Game Over!', 'Press any key to close game.')
-#             game.bye()
-#             break
-#     player_obj.move_laser()
-#     player_obj.is_laser_gone()
+import turtle
+import random
+import math
+import json
+from border import Border
+from player import Player
+from invader import Invader
+
+
+# User input player name and difficulty level.
+player_name_input = turtle.textinput('Enter player name: ', 'Your name:')
+while True:
+    level_input = turtle.textinput('Choose difficulty',  '(Easy, Normal, Hard):')
+    if level_input.upper() == 'EASY':
+        mode_input = 10
+        break
+    elif level_input.upper() == 'NORMAL':
+        mode_input = 15
+        Player().laser_speed = 80
+        break
+    elif level_input.upper() == 'HARD':
+        mode_input = 20
+        Player().laser_speed = 120
+        break
+    else:
+        print('Please Try again.')
+
+border_obj = Border()
+player_obj = Player()
+invader_obj = Invader()
+
+# Register shapes to turtle.
+turtle.register_shape("player.gif")
+turtle.register_shape("invader.gif")
+
+# Set up screen.
+game = turtle.Screen()
+game.title('Space Invaders')
+game.bgcolor('black')
+game.bgpic('background.gif')
+game.tracer(1)
+
+# Set up border, write game score.
+border_obj.draw()
+border_obj.game_score(player_name_input)
+
+# Set position of player, laser and group of invaders.
+player_obj.set_icon_position()
+player_obj.set_laser_position()
+invader_army = list(invader_obj.set_invaders(mode_input))
+
+# Binding key to control player icon and firing option.
+turtle.listen()
+turtle.onkey(player_obj.move_left, "Left")
+turtle.onkey(player_obj.move_right, "Right")
+turtle.onkey(player_obj.fire, "space")
+
+
+def collision_check(p1, p2):
+    distance = math.sqrt(math.pow(p1.xcor() - p2.xcor(), 2) + math.pow(p1.ycor() - p2.ycor(), 2))
+    if distance < 30:
+        return True
+    else:
+        return False
+
+
+# Main game loop
+while True:
+    game.update()
+    for invader in invader_army:
+        invader_obj.move(invader_army, invader)
+        # Check if collision between laser and invader.
+        if collision_check(player_obj.laser, invader):
+            border_obj.score += 10
+            game_score = "Score: %s" % border_obj.score
+            border_obj.score_pen.clear()
+            border_obj.score_pen.write(game_score, False, align="left", font=("Mono", 14, "bold"))
+            # Reset laser position and status.
+            player_obj.laser.hideturtle()
+            player_obj.laser_status = "ready"
+            player_obj.laser.setposition(player_obj.icon.xcor(), player_obj.icon.ycor())
+            # Reset invader position.
+            x = random.randint(-250, 200)
+            y = random.randint(100, 250)
+            invader.setposition(x, y)
+        # Check if collision between player icon and invader.
+        if collision_check(player_obj.icon, invader):
+            player_obj.icon.hideturtle()
+            invader.hideturtle()
+            # Write save data file as json file
+            save_slot = {
+                'data_slot': {
+                    'Player name': player_name_input.upper(),
+                    'Difficulty': level_input.upper(),
+                    'Score': border_obj.score
+                }
+            }
+            with open('leaderboard.json', 'a') as leaderboard_file:
+                json.dump(save_slot, leaderboard_file, indent=4)
+            game_over = turtle.textinput('Game Over!', 'Press any key to close game.')
+            game.bye()
+            break
+    player_obj.move_laser()
+    player_obj.is_laser_gone()
